a3b5c233c006ff777f6c896f4fd75aa5590197fc
w <none>
2016-03-06 16:46:25 -0800

    add tests

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+test -z "$(UZBL_URI=http://www.example.com/ ./check-tracking http://www.example.org/)"
+test "$(UZBL_URI=http://www.example.com/ ./check-tracking http://ad.doubleclick.net/)" = about:blank
+test -z "$(UZBL_URI=https://www.google.com/ ./check-tracking http://ad.doubleclick.net/)"
+test "$(UZBL_URI=https://www.google.com/ ./check-tracking https://www.facebook.com/)" = about:blank
